e00a6777a26ce2c50a6652aeadccbbe4d46290c3
Akashkumar7375 <127717664+Akashkumar7375@users.noreply.github.com>
2024-07-10 15:17:39 +0530

    Update multer.js

--- Api/middleware/multer.js
+++ Api/middleware/multer.js
@@ -4,7 +4,7 @@ const multer=require('multer')
 let storage=multer.diskStorage({
 
     destination:function(req,file,cb){
-        cb(null,'../shop/public/uploads')
+        cb(null,'../shopclone/public/uploads')
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+file.originalname)
